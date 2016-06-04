490c094b0a4b75c37b72512b1730aac1d0000258
dormon <imilet@fit.vutbr.cz>
2016-06-04 19:41:55 +0200

    Added kernel

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,6 @@
 #include<assimp/postprocess.h>
 
 struct Data{
-  std::shared_ptr<ge::de::TypeRegister>             typeRegister        = nullptr;
-  std::shared_ptr<ge::de::FunctionRegister>         functionRegister    = nullptr;
-  std::shared_ptr<ge::de::NameRegister>             nameRegister        = nullptr;
-  std::shared_ptr<ge::de::VariableRegister>         variableRegister    = nullptr;
   ge::de::Kernel kernel;
   std::shared_ptr<ge::gl::opengl::FunctionProvider> gl                  = nullptr;
   std::shared_ptr<ge::util::SDLEventProc>           mainLoop            = nullptr;
@@ -51,7 +47,6 @@ struct Data{
       virtual ~WindowEventHandler(){}
   };
   std::shared_ptr<ge::de::Resource>model = nullptr;
-  std::shared_ptr<ge::de::Resource>program = nullptr;
   std::shared_ptr<ge::de::Function>prg = nullptr;
 };
 
@@ -106,8 +101,12 @@ std::string shaderSourceLoader(
   return ss.str();
 }
 
-std::shared_ptr<ge::gl::Shader>createShader(GLenum type,std::string source){
-  return std::make_shared<ge::gl::Shader>(type,source);
+std::shared_ptr<ge::gl::Shader>createVertexShader(std::string source){
+  return std::make_shared<ge::gl::Shader>(GL_VERTEX_SHADER,source);
+}
+
+std::shared_ptr<ge::gl::Shader>createFragmentShader(std::string source){
+  return std::make_shared<ge::gl::Shader>(GL_FRAGMENT_SHADER,source);
 }
 
 std::shared_ptr<ge::gl::Program>createProgram2(
@@ -121,14 +120,16 @@ int main(int argc,char*argv[]){
   //std::cout<<shaderSourceLoader("#version 450\n","","shaders/","vertex.vp\nfragment.fp");
   //return 0;
   Data data;
+  /*
   data.typeRegister     = std::make_shared<ge::de::TypeRegister>();
   data.nameRegister     = std::make_shared<ge::de::NameRegister>();
   data.functionRegister = std::make_shared<ge::de::FunctionRegister>(data.typeRegister,data.nameRegister);
   ge::de::registerStdFunctions(data.functionRegister);
   data.variableRegister = std::make_shared<ge::de::VariableRegister>("*");
+  */
   auto argm = std::make_shared<ge::util::ArgumentManager>(argc-1,argv+1);
-  ge::util::copyArgumentManager2VariableRegister(data.variableRegister,*argm,data.functionRegister);
-  std::cout<<data.variableRegister->toStr(0,data.typeRegister)<<std::endl;
+  ge::util::copyArgumentManager2VariableRegister(data.kernel.variableRegister,*argm,data.kernel.functionRegister);
+  std::cout<<data.kernel.variableRegister->toStr(0,data.kernel.typeRegister)<<std::endl;
 
   data.mainLoop = std::make_shared<ge::util::SDLEventProc>(true);
   data.mainLoop->setIdleCallback(std::make_shared<Data::IdleCallback>(&data));
@@ -150,7 +151,7 @@ void Data::IdleCallback::operator()(){
 
   this->data->emptyVAO->bind();
   (*this->data->prg)();
-  (*(std::shared_ptr<ge::gl::Program>*)*this->data->program)->use();
+  (*(std::shared_ptr<ge::gl::Program>*)*this->data->prg->getOutputData())->use();
   this->data->gl->glDrawArrays(GL_TRIANGLE_STRIP,0,3);
   this->data->emptyVAO->unbind();
 
@@ -192,105 +193,51 @@ void Data::init(Data*data){
   TwInit(TW_OPENGL_CORE,nullptr);
   TwWindowSize(data->window->getWidth(),data->window->getHeight());
 
-  data->variableManipulator = std::make_shared<VariableRegisterManipulator>(data->variableRegister);
+  auto &kernel = data->kernel;
+  data->variableManipulator = std::make_shared<VariableRegisterManipulator>(kernel.variableRegister);
   //so part
-  data->typeRegister->addAtomicType(
+  kernel.addAtomicType(
       "AssimpScene",
       sizeof(aiScene const*),
       nullptr,
       [](void*ptr){aiReleaseImport(*((aiScene const**)ptr));});
-  data->typeRegister->addAtomicType(
+  kernel.addAtomicType(
       "SharedShader",
       sizeof(std::shared_ptr<ge::gl::Shader>),
       [](void*ptr){new(ptr)std::shared_ptr<ge::gl::Shader>;},
       [](void*ptr){((std::shared_ptr<ge::gl::Shader>*)ptr)->~shared_ptr();});
-  data->typeRegister->addAtomicType(
+  kernel.addAtomicType(
       "SharedProgram",
       sizeof(std::shared_ptr<ge::gl::Program>),
       [](void*ptr){new(ptr)std::shared_ptr<ge::gl::Program>;},
       [](void*ptr){((std::shared_ptr<ge::gl::Program>*)ptr)->~shared_ptr();});
-  ge::de::registerBasicFunction(data->functionRegister,"assimpLoader"      ,assimpLoader      );
-  auto fid = ge::de::registerBasicFunction(data->functionRegister,"shaderSourceLoader",shaderSourceLoader);
-  data->nameRegister->setFceInputName(fid,0,"version");
-  data->nameRegister->setFceInputName(fid,1,"defines");
-  data->nameRegister->setFceInputName(fid,2,"dir");
-  data->nameRegister->setFceInputName(fid,3,"fileNames");
-  data->nameRegister->setFceOutputName(fid,"source");
-  fid = ge::de::registerBasicFunction(data->functionRegister,"createShader"      ,createShader      );
-  data->nameRegister->setFceInputName(fid,0,"type");
-  data->nameRegister->setFceInputName(fid,1,"source");
-  data->nameRegister->setFceOutputName(fid,"sharedShader");
-  fid = ge::de::registerBasicFunction(data->functionRegister,"createProgram2"    ,createProgram2    );
-  data->nameRegister->setFceInputName(fid,0,"shader0");
-  data->nameRegister->setFceInputName(fid,1,"shader1");
-  data->nameRegister->setFceOutputName(fid,"sharedProgram");
+
+  kernel.addFunction({"assimpLoader"},assimpLoader);
+  kernel.addFunction({"shaderSourceLoader","source","version","defines","dir","fileNames"},shaderSourceLoader);
+  kernel.addFunction({"createVertexShader","sharedShader","source"},createVertexShader);
+  kernel.addFunction({"createFragmentShader","sharedShader","source"},createFragmentShader);
+  kernel.addFunction({"createProgram2","shaderProgram","shader0","shader1"},createProgram2);
 
 
   //script part
-  data->model = data->typeRegister->sharedResource("AssimpScene");
-  auto fceLoader = data->functionRegister->sharedFunction("assimpLoader");
-  fceLoader->bindInput(data->functionRegister,0,data->variableRegister->getVariable("modelFile"));
-  fceLoader->bindOutput(data->functionRegister,data->model);
+  auto fceLoader = kernel.createFunction("assimpLoader",{"modelFile"},"AssimpScene");
 
   (*fceLoader)();
-  std::cout<<(*((aiScene const**)*data->model))->mNumMeshes<<std::endl;
+  std::cout<<(*((aiScene const**)*fceLoader->getOutputData()))->mNumMeshes<<std::endl;
 
-  auto version = data->functionRegister->sharedFunction("Nullary");
-  version->bindOutput(data->functionRegister,data->typeRegister->sharedResource("string"));
-  *(std::string*)*version->getOutputData() = "#version 450\n";
+  kernel.addVariable("program.version",std::string("#version 450\n"));
+  kernel.addVariable("program.vertexShader",std::string("vertex.vp"));
+  kernel.addVariable("program.fragmentShader",std::string("fragment.fp"));
+  kernel.addVariable("program.defines",std::string(""));
 
-  auto vp = data->functionRegister->sharedFunction("Nullary");
-  vp->bindOutput(data->functionRegister,data->typeRegister->sharedResource("string"));
-  *(std::string*)*vp->getOutputData() = "vertex.vp";
+  auto vpl = kernel.createFunction("shaderSourceLoader",{"program.version","program.defines","shaderDirectory","program.vertexShader"},"string");
+  auto fpl = kernel.createFunction("shaderSourceLoader",{"program.version","program.defines","shaderDirectory","program.fragmentShader"},"string");
 
-  auto fp = data->functionRegister->sharedFunction("Nullary");
-  fp->bindOutput(data->functionRegister,data->typeRegister->sharedResource("string"));
-  *(std::string*)*fp->getOutputData() = "fragment.fp";
+  auto vpc = kernel.createFunction("createVertexShader",{vpl},"SharedShader");
+  auto fpc = kernel.createFunction("createFragmentShader",{fpl},"SharedShader");
 
-  auto defines = data->functionRegister->sharedFunction("Nullary");
-  defines->bindOutput(data->functionRegister,data->typeRegister->sharedResource("string"));
-  *(std::string*)*defines->getOutputData() = "";
+  data->prg = kernel.createFunction("createProgram2",{vpc,fpc},"SharedProgram");
 
-  auto vpl = data->functionRegister->sharedFunction("shaderSourceLoader");
-  vpl->bindInput(data->functionRegister,0,version);
-  vpl->bindInput(data->functionRegister,1,defines);
-  vpl->bindInput(data->functionRegister,2,data->variableRegister->getVariable("shaderDirectory"));
-  vpl->bindInput(data->functionRegister,3,vp);
-  vpl->bindOutput(data->functionRegister,data->typeRegister->sharedResource("string"));
-  auto fpl = data->functionRegister->sharedFunction("shaderSourceLoader");
-  fpl->bindInput(data->functionRegister,0,version);
-  fpl->bindInput(data->functionRegister,1,defines);
-  fpl->bindInput(data->functionRegister,2,data->variableRegister->getVariable("shaderDirectory"));
-  fpl->bindInput(data->functionRegister,3,fp);
-  fpl->bindOutput(data->functionRegister,data->typeRegister->sharedResource("string"));
-
-  auto vpen = data->functionRegister->sharedFunction("Nullary");
-  vpen->bindOutput(data->functionRegister,data->typeRegister->sharedResource("u32"));
-  *(GLenum*)*vpen->getOutputData() = GL_VERTEX_SHADER;
-
-  auto fpen = data->functionRegister->sharedFunction("Nullary");
-  fpen->bindOutput(data->functionRegister,data->typeRegister->sharedResource("u32"));
-  *(GLenum*)*fpen->getOutputData() = GL_FRAGMENT_SHADER;
-
-
-  auto vpc = data->functionRegister->sharedFunction("createShader");
-  vpc->bindInput(data->functionRegister,0,vpen);
-  vpc->bindInput(data->functionRegister,1,vpl);
-  vpc->bindOutput(data->functionRegister,data->typeRegister->sharedResource("SharedShader"));
-
-  auto fpc = data->functionRegister->sharedFunction("createShader");
-  fpc->bindInput(data->functionRegister,0,fpen);
-  fpc->bindInput(data->functionRegister,1,fpl);
-  fpc->bindOutput(data->functionRegister,data->typeRegister->sharedResource("SharedShader"));
-
-  data->prg = data->functionRegister->sharedFunction("createProgram2");
-  data->prg->bindInput(data->functionRegister,0,vpc);
-  data->prg->bindInput(data->functionRegister,1,fpc);
-  data->program = data->typeRegister->sharedResource("SharedProgram");
-  data->prg->bindOutput(data->functionRegister,data->program);
-
-  //data->model = nullptr;
-  //fceLoader->bindOutput(data->functionRegister,nullptr);
 }
 
 void Data::deinit(Data*data){
