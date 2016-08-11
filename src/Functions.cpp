53e7f3125ba5e5b7e8aa50b336941f5ec53fd9c8
dormon <imilet@fit.vutbr.cz>
2016-08-11 03:16:46 +0200

    Added Functions.cpp Added glUseProgram into prg2

--- src/Functions.cpp
+++ src/Functions.cpp
@@ -1,11 +1,90 @@
 #include<Functions.h>
 
 #include<geDE/Kernel.h>
+#include<geDE/RegisterBasicFunction.h>
+#include<geCore/Text.h>
+#include<geGL/geGL.h>
 
 using namespace ge::de;
 
+namespace ge{
+  namespace de{
+    GE_DE_ADD_KEYWORD(ge::gl::Program,"Program")
+    GE_DE_ADD_KEYWORD(std::shared_ptr<ge::gl::Shader>,"SharedShader")
+    GE_DE_ADD_KEYWORD(std::shared_ptr<ge::gl::Program>,"SharedProgram")
+  }
+}
 
+std::string shaderSourceLoader(
+    std::string const&version,
+    std::string const&defines,
+    std::string const&dir,
+    std::string const&fileNames){
+  std::cout<<"("<<version<<"#"<<defines<<"#"<<dir<<"#"<<fileNames<<")"<<std::endl;
+  std::stringstream ss;
+  ss<<version;
+  ss<<defines;
+  size_t pos=0;
+  do{
+    size_t newPos = fileNames.find("\n",pos);
+    if(newPos == std::string::npos){
+      auto fileName = fileNames.substr(pos);
+      ss<<ge::core::loadTextFile(dir+fileName);
+      break;
+    }
+    auto fileName = fileNames.substr(pos,newPos-pos);
+    ss<<ge::core::loadTextFile(dir+fileName);
+    pos = newPos+1;
+  }while(true);
+  return ss.str();
+}
+
+template<GLenum TYPE>
+std::shared_ptr<ge::gl::Shader>createShader(std::string source){
+  std::cout<<"createShader: "<<TYPE<<std::endl;
+  return std::make_shared<ge::gl::Shader>(TYPE,source);
+}
+
+template<typename...SHADERS>
+std::shared_ptr<ge::gl::Program>createProgram(
+    SHADERS...shaders){
+  std::cout<<"createProgram"<<std::endl;
+  return std::make_shared<ge::gl::Program>(shaders...);
+}
+
+template<typename T>inline T*sharedPointerToPointer(std::shared_ptr<T>const&p){return &*p;}
+
+int32_t neco(){
+  return 10;
+}
 
 void registerPlugin(Kernel*kernel){
   (void)kernel;
+  kernel->addAtomicClass<std::shared_ptr<ge::gl::Shader>>("SharedShader");
+  kernel->addAtomicClass<std::shared_ptr<ge::gl::Program>>("SharedProgram");
+  kernel->addAtomicClass<ge::gl::Program>("Program");
+  kernel->typeRegister->addType<ge::gl::Program*>();
+
+
+  kernel->addFunction({"shaderSourceLoader","source","version","defines","dir","fileNames"},shaderSourceLoader);
+  kernel->addFunction({"createVertexShader"    ,"sharedShader","source"},createShader<GL_VERTEX_SHADER>);
+  kernel->addFunction({"createFragmentShader"  ,"sharedShader","source"},createShader<GL_FRAGMENT_SHADER>);
+  kernel->addFunction({"createGeometryShader"  ,"sharedShader","source"},createShader<GL_GEOMETRY_SHADER>);
+  kernel->addFunction({"createControlShader"   ,"sharedShader","source"},createShader<GL_TESS_CONTROL_SHADER>);
+  kernel->addFunction({"createEvaluationShader","sharedShader","source"},createShader<GL_TESS_EVALUATION_SHADER>);
+  kernel->addFunction({"createComputeShader"   ,"sharedShader","source"},createShader<GL_COMPUTE_SHADER>);
+  
+
+
+  using SS = std::shared_ptr<ge::gl::Shader>const&;
+  kernel->addFunction({"createProgram1","shaderProgram","shader0"},createProgram<SS>);
+  kernel->addFunction({"createProgram2","shaderProgram","shader0","shader1"},createProgram<SS,SS>);
+  kernel->addFunction({"createProgram3","shaderProgram","shader0","shader1","shader2"},createProgram<SS,SS,SS>);
+  kernel->addFunction({"createProgram4","shaderProgram","shader0","shader1","shader2","shader3"},createProgram<SS,SS,SS,SS>);
+  kernel->addFunction({"createProgram5","shaderProgram","shader0","shader1","shader2","shader3","shader4"},createProgram<SS,SS,SS,SS,SS>);
+
+
+  kernel->addFunction({"sharedProgram2Program*","program*","sharedProgram"},sharedPointerToPointer<ge::gl::Program>);
+  kernel->addFunction({"Program::use"},&ge::gl::Program::use);
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -25,14 +25,15 @@
 
 
 #include<RegisterKeyboard.h>
+#include<Functions.h>
 
 struct Data{
   ge::de::Kernel kernel;
-  std::shared_ptr<ge::gl::Context>    gl       = nullptr;
-  std::shared_ptr<ge::ad::SDLMainLoop>mainLoop = nullptr;
-  std::shared_ptr<ge::ad::SDLWindow>  window   = nullptr;
-  std::shared_ptr<ge::gl::VertexArray>             emptyVAO            = nullptr;
-  std::shared_ptr<VariableRegisterManipulator>     variableManipulator = nullptr;
+  std::shared_ptr<ge::gl::Context>            gl                  = nullptr;
+  std::shared_ptr<ge::ad::SDLMainLoop>        mainLoop            = nullptr;
+  std::shared_ptr<ge::ad::SDLWindow>          window              = nullptr;
+  std::shared_ptr<ge::gl::VertexArray>        emptyVAO            = nullptr;
+  std::shared_ptr<VariableRegisterManipulator>variableManipulator = nullptr;
   static void init(Data*data);
   static void deinit(Data*data);
   class IdleCallback: public ge::ad::SDLCallbackInterface{
@@ -71,9 +72,6 @@ struct Data{
       virtual ~KeyUpCallback(){}
   };
 
-  std::shared_ptr<ge::de::Resource>model = nullptr;
-  //std::shared_ptr<ge::de::Statement>prg = nullptr;
-  //std::shared_ptr<ge::de::Function>prg = nullptr;
   std::shared_ptr<ge::de::Statement>prg2 = nullptr;
   std::shared_ptr<ge::de::Function>blafce = nullptr;
   std::shared_ptr<ge::de::Function>modelFce = nullptr;
@@ -102,9 +100,6 @@ class AssimpModel{
 namespace ge{
   namespace de{
     GE_DE_ADD_KEYWORD(std::shared_ptr<AssimpModel>,"SharedAssimpModel")
-    GE_DE_ADD_KEYWORD(ge::gl::Program,"Program")
-    GE_DE_ADD_KEYWORD(std::shared_ptr<ge::gl::Shader>,"SharedShader")
-    GE_DE_ADD_KEYWORD(std::shared_ptr<ge::gl::Program>,"SharedProgram")
     GE_DE_ADD_KEYWORD(glm::vec3,ge::de::keyword<float[3]>())
     GE_DE_ADD_KEYWORD(glm::vec4,ge::de::keyword<float[4]>())
     GE_DE_ADD_KEYWORD(glm::mat3,ge::de::keyword<float[3][3]>())
@@ -138,47 +133,6 @@ bool addOneToXIfSignaling(glm::vec3,bool trigger){
 
 
 
-std::string shaderSourceLoader(
-    std::string const&version,
-    std::string const&defines,
-    std::string const&dir,
-    std::string const&fileNames){
-  std::cout<<"("<<version<<"#"<<defines<<"#"<<dir<<"#"<<fileNames<<")"<<std::endl;
-  std::stringstream ss;
-  ss<<version;
-  ss<<defines;
-  size_t pos=0;
-  do{
-    size_t newPos = fileNames.find("\n",pos);
-    if(newPos == std::string::npos){
-      auto fileName = fileNames.substr(pos);
-      ss<<ge::core::loadTextFile(dir+fileName);
-      break;
-    }
-    auto fileName = fileNames.substr(pos,newPos-pos);
-    ss<<ge::core::loadTextFile(dir+fileName);
-    pos = newPos+1;
-  }while(true);
-  return ss.str();
-}
-
-std::shared_ptr<ge::gl::Shader>createVertexShader(std::string source){
-  std::cout<<"createVertexShader"<<std::endl;
-  return std::make_shared<ge::gl::Shader>(GL_VERTEX_SHADER,source);
-}
-
-std::shared_ptr<ge::gl::Shader>createFragmentShader(std::string source){
-  std::cout<<"createFragmentShader"<<std::endl;
-  return std::make_shared<ge::gl::Shader>(GL_FRAGMENT_SHADER,source);
-}
-
-std::shared_ptr<ge::gl::Program>createProgram2(
-    std::shared_ptr<ge::gl::Shader>s0,
-    std::shared_ptr<ge::gl::Shader>s1){
-  std::cout<<"createProgram"<<std::endl;
-  return std::make_shared<ge::gl::Program>(s0,s1);
-}
-
 int main(int argc,char*argv[]){
   Data data;
   auto argm = std::make_shared<ge::util::ArgumentManager>(argc-1,argv+1);
@@ -207,10 +161,10 @@ void Data::IdleCallback::operator()(){
 
   this->data->emptyVAO->bind();
   //(*this->data->prg)();
+  this->data->gl->glUseProgram(0);
   (*this->data->prg2)();
   auto&program = ((std::shared_ptr<ge::gl::Program>&)*this->data->prg2->toBody()->at(0)->toFunction()->getOutputData());
-  program->use();
-  //program->setMatrix4fv("projection",(float*)this->data->kernel.variable("camera.projection")->getData());
+  //program->use();
   program->set3fv("position",(float*)this->data->kernel.variable("camera.position")->getData());
   this->data->gl->glDrawArrays(GL_TRIANGLE_STRIP,0,3);
   this->data->emptyVAO->unbind();
@@ -283,83 +237,78 @@ void Data::init(Data*data){
   TwWindowSize(data->window->getWidth(),data->window->getHeight());
 
   auto &kernel = data->kernel;
+  registerPlugin(&kernel);
   kernel.addAtomicType(
       "SharedAssimpModel",
       sizeof(std::shared_ptr<AssimpModel>),
       nullptr,
       [](void*ptr){((std::shared_ptr<AssimpModel>*)ptr)->~shared_ptr();});
 
-  kernel.addAtomicClass<std::shared_ptr<ge::gl::Shader>>("SharedShader");
-  kernel.addAtomicClass<std::shared_ptr<ge::gl::Program>>("SharedProgram");
-
   kernel.addAtomicType("GL",sizeof(GL),[](void*ptr){new(ptr)GL();},[](void*ptr){((GL*)ptr)->~GL();});
 
   kernel.addFunction({"assimpLoader","fileName"},assimpLoader);
-  kernel.addFunction({"shaderSourceLoader","source","version","defines","dir","fileNames"},shaderSourceLoader);
-  kernel.addFunction({"createVertexShader","sharedShader","source"},createVertexShader);
-  kernel.addFunction({"createFragmentShader","sharedShader","source"},createFragmentShader);
-  kernel.addFunction({"createProgram2","shaderProgram","shader0","shader1"},createProgram2);
   kernel.addFunction({"bla","output","input"},bla);
-
   kernel.addFunction({"addOneToX","output","input"},addOneToX);
-
   kernel.addFunction({"addOneToXIf","output","input"},addOneToXIf,addOneToXIfSignaling);
   //script part
-
+  kernel.addArrayType("vec3",3,"f32");
+  kernel.addArrayType("mat4",16,"f32");
 
   kernel.addVariable("program.version"       ,std::string("#version 450\n"));
   kernel.addVariable("program.vertexShader"  ,std::string("vertex.vp")     );
   kernel.addVariable("program.fragmentShader",std::string("fragment.fp")   );
   kernel.addVariable("program.defines"       ,std::string("")              );
-
-  /*
-     auto vpl = kernel.createFunction("shaderSourceLoader",{"program.version","program.defines","shaderDirectory","program.vertexShader"},"string");
-     auto fpl = kernel.createFunction("shaderSourceLoader",{"program.version","program.defines","shaderDirectory","program.fragmentShader"},"string");
-
-     auto vpc = kernel.createFunction("createVertexShader"  ,{vpl},"SharedShader");
-     auto fpc = kernel.createFunction("createFragmentShader",{fpl},"SharedShader");
-
-     data->prg = kernel.createFunction("createProgram2",{vpc,fpc},"SharedProgram");
-     */
-
-  auto a = kernel.createFunctionNodeFactory("vertexShaderSourceLoader","shaderSourceLoader");
-  auto b = kernel.createFunctionNodeFactory("fragmentShaderSourceLoader","shaderSourceLoader");
-  auto aa = kernel.createFunctionNodeFactory("createVertexShaderFromSource","createVertexShader",{a});
-  auto bb = kernel.createFunctionNodeFactory("createFragmentShaderFromSource","createFragmentShader",{b});
-  auto c = kernel.createFunctionNodeFactory("createProgramFromVertexFragment","createProgram2",{aa,bb});
-  auto fac = kernel.createCompositeFunctionFactory("createVSFSProgram",c,{{a,b},{a,b},{a},{a},{b},{b}},{{0,0},{2,2},{1},{3},{1},{3}});
-  kernel.addCompositeFunction({"sharedProgram","version","vsDefines","vertexSourceFiles","fsDefines","fragmentSourceFiles"},fac);
-
-  //data->prg = kernel.createFunction("createVSFSProgram",{"program.version","shaderDirectory","program.defines","program.vertexShader","program.defines","program.fragmentShader"});
-
-
-  kernel.addArrayType("vec3",3,"f32");
-  kernel.addArrayType("mat4",16,"f32");
   kernel.addVariable("camera.position",glm::vec3(0.f));
-
-
   kernel.addVariable("camera.fovy",glm::radians<float>(90.f));
   kernel.addVariable("camera.near",1.f);
   kernel.addVariable("camera.far",1000.f);
   kernel.addVariable("camera.aspect",1.f);
   kernel.addVariable("camera.projection",glm::mat4(1.f));
-
   kernel.addVariable("shaderDirectory",std::string("shaders/"));
   kernel.addVariable("testString",std::string("ahoj"));
   kernel.addVariable("modelFile",std::string("/media/windata/ft/prace/models/cube/cube.obj"));
-
-  kernel.addFunction({"computeProjection","projectionMatrix","fovy","aspect","near","far"},glm::perspective<float>);
-
-  keyboard::registerKeyboard(&kernel);
-
   kernel.addEmptyVariable("gl","GL");
   kernel.addVariable("transpose",(int32_t)GL_FALSE);
   kernel.addVariable("nofMatrices",(uint32_t)1);
+  keyboard::registerKeyboard(&kernel);
+
+  {
+    auto a = kernel.createFunctionNodeFactory("vertexShaderSourceLoader","shaderSourceLoader");
+    auto b = kernel.createFunctionNodeFactory("fragmentShaderSourceLoader","shaderSourceLoader");
+    auto aa = kernel.createFunctionNodeFactory("createVertexShaderFromSource","createVertexShader",{a});
+    auto bb = kernel.createFunctionNodeFactory("createFragmentShaderFromSource","createFragmentShader",{b});
+    auto c = kernel.createFunctionNodeFactory("createProgramFromVertexFragment","createProgram2",{aa,bb});
+    auto fac = kernel.createCompositeFunctionFactory("createVSFSProgram",c,{{a,b},{a,b},{a},{a},{b},{b}},{{0,0},{2,2},{1},{3},{1},{3}});
+    kernel.addCompositeFunction({"sharedProgram","version","vsDefines","vertexSourceFiles","fsDefines","fragmentSourceFiles"},fac);
+  }
+  {
+    auto a = kernel.createFunctionNodeFactory("vertexShaderSourceLoader","shaderSourceLoader");
+    auto b = kernel.createFunctionNodeFactory("geometryShaderSourceLoader","shaderSourceLoader");
+    auto c = kernel.createFunctionNodeFactory("fragmentShaderSourceLoader","shaderSourceLoader");
+    auto aa = kernel.createFunctionNodeFactory("createVertexShaderFromSource","createVertexShader",{a});
+    auto bb = kernel.createFunctionNodeFactory("createGeometryShaderFromSource","createGeometryShader",{b});
+    auto cc = kernel.createFunctionNodeFactory("createFragmentShaderFromSource","createFragmentShader",{c});
+    auto d = kernel.createFunctionNodeFactory("createProgramFromVSGSFS","createProgram3",{aa,bb,cc});
+    auto fac = kernel.createCompositeFunctionFactory("createVSGSFSProgram",d,{{a,b,c},{a,b,c},{a},{a},{b},{b},{c},{c}},{{0,0,0},{2,2,2},{1},{3},{1},{3},{1},{3}});
+    kernel.addCompositeFunction({"sharedProgram","version","vsDefines","vsSourceFiles","gsDefines","gsSourceFiles","fsDefines","fsSourceFiles"},fac);
+  }
+
+
+  kernel.addFunction({"computeProjection","projectionMatrix","fovy","aspect","near","far"},glm::perspective<float>);
+
 
   data->prg2 = std::make_shared<ge::de::Body>();
-  data->prg2->toBody()->addStatement(kernel.createFunction("createVSFSProgram",{"program.version","shaderDirectory","program.defines","program.vertexShader","program.defines","program.fragmentShader"}));
-  data->prg2->toBody()->addStatement(kernel.createFunction("addOneToXIf",{"camera.position","keyboard.A"},"camera.position"));
-  data->prg2->toBody()->addStatement(kernel.createFunction("computeProjection",{"camera.fovy","camera.aspect","camera.near","camera.far"},"camera.projection"));
+  data->prg2->toBody()->addStatement(kernel.createFce("createVSFSProgram","program.version","shaderDirectory","program.defines","program.vertexShader","program.defines","program.fragmentShader"));
+
+  auto usep = kernel.createFce("Program::use",kernel.createFce("sharedProgram2Program*",data->prg2->toBody()->at(0)->toFunction()->getOutputData()));
+  usep->setIgnoreDirty(true);
+  usep->setIgnoreInputChanges(true);
+  data->prg2->toBody()->addStatement(usep);
+
+
+  data->prg2->toBody()->addStatement(kernel.createFce("addOneToXIf","camera.position","keyboard.A","camera.position"));
+  data->prg2->toBody()->addStatement(kernel.createFce("computeProjection","camera.fovy","camera.aspect","camera.near","camera.far","camera.projection"));
+  data->prg2->setIgnoreDirty(true);
 
   data->variableManipulator = std::make_shared<VariableRegisterManipulator>(kernel.variableRegister);
 
@@ -370,6 +319,5 @@ void Data::init(Data*data){
 void Data::deinit(Data*data){
   (void)data;
   data->variableManipulator = nullptr;
-  data->model = nullptr;
   TwTerminate();
 }
