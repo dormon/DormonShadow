ffb2b4c68f32a73791b5ad30a8162398e6609b38
dormon <imilet@fit.vutbr.cz>
2016-08-18 21:28:27 +0200

    Working version of UI

--- src/GEDEEditor.cpp
+++ src/GEDEEditor.cpp
@@ -520,37 +520,32 @@ namespace ui{
 
 
 void Function::create(){
-  this->captionMargin = 10;
-
-  /*
-     this->node = this->draw2D->createNode();
-     using namespace ui;
-     auto root = new Split<1>({
-     new Rectangle(0,this->lineWidth,{new Line(0,.5,1,.5,this->lineWidth,this->lineColor)}),//top line
-     new Split<0>({
-     new Rectangle(this->lineWidth,0,{new Line(.5,0,.5,1,this->lineWidth,this->lineColor)}),//left line
-     new Split<1>({
-     new Split<1>({
-     new Rectangle(0,this->captionMargin*0+100),
-     new Split<0>({
-     new Rectangle(this->captionMargin*0+100,0),
-     new Rectangle(this->captionFontSize*this->functionName.length(),this->captionFontSize,{new Text(this->functionName,this->captionFontSize,this->captionColor)}),
-     new Rectangle(this->captionMargin*0+100,0),
-     }),
-     new Rectangle(0,this->captionMargin*0+100),
-     }),//,{new Triangle(0,0,1,0,1,1,this->captionBackgrounColor),new Triangle(0,0,1,1,0,1,this->captionBackgrounColor)}),
-     new Rectangle(0,this->lineWidth,{new Line(0,.5,1,.5,this->lineWidth,this->lineColor)}),//caption line
-     }),
-     new Rectangle(this->lineWidth,0,{new Line(.5,0,.5,1,this->lineWidth,this->lineColor)}),//right line
-     }),
-     new Rectangle(0,this->lineWidth,{new Line(0,.5,1,.5,this->lineWidth,this->lineColor)}),});//bottom line
-     root->addToNode(this->draw2D,node);
-     delete root;
-     */
-
-
   this->node = this->draw2D->createNode();
   using namespace ui;
+
+#if 1
+  auto root = new Split<1>({
+      new Rectangle(0,this->lineWidth,{new Line(0,.5,1,.5,this->lineWidth,this->lineColor)}),//top line
+      new Split<0>({
+        new Rectangle(this->lineWidth,0,{new Line(.5,0,.5,1,this->lineWidth,this->lineColor)}),//left line
+        new Split<1>({
+          new Split<1>({
+            new Rectangle(0,this->captionMargin),
+            new Split<0>({
+              new Rectangle(this->captionMargin,0),
+              new Rectangle(this->captionFontSize*this->functionName.length(),this->captionFontSize*2,{new Text(this->functionName,this->captionFontSize,this->captionColor)}),
+              new Rectangle(this->captionMargin,0),
+              }),
+            new Rectangle(0,this->captionMargin),
+            },{new Triangle(0,0,1,0,1,1,this->captionBackgrounColor),new Triangle(0,0,1,1,0,1,this->captionBackgrounColor)}),
+        new Rectangle(0,this->lineWidth,{new Line(0,.5,1,.5,this->lineWidth,this->lineColor)}),//caption line
+        }),
+      new Rectangle(this->lineWidth,0,{new Line(.5,0,.5,1,this->lineWidth,this->lineColor)}),//right line
+      }),
+       new Rectangle(0,this->lineWidth,{new Line(0,.5,1,.5,this->lineWidth,this->lineColor)}),});//bottom line
+  root->addToNode(this->draw2D,node);
+  delete root;
+#endif
 
 #if 0
   //Vertical SPLIT TEST
@@ -578,7 +573,7 @@ void Function::create(){
   delete root;
 #endif
 
-#if 1
+#if 0
   //HORIZONTAL THAN VERTICAL SPLIT TEST
   auto root = new Split<0>({
       new Rectangle(this->lineWidth,0,{new Line(.5,0,.5,1,this->lineWidth)}),//left line
@@ -589,7 +584,7 @@ void Function::create(){
         new Rectangle(4*this->captionFontSize,this->captionFontSize*2,{new Text("AHOJ",8)}),
         new Rectangle(0,30),
         new Rectangle(0,this->lineWidth,{new Line(0,.5,1,.5,this->lineWidth)}),//bottom line
-      }),
+        }),
       new Rectangle(30,0),
       new Rectangle(this->lineWidth,0,{new Line(.5,0,.5,1,this->lineWidth)}),//left line
       });
